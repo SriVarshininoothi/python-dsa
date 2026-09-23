d47816e427c44f0d13eba04baeeb1872c049ef93
SriVarshininoothi <noothi.srivarshini982@gmail.com>
2026-09-23 10:55:08 +0530

    Created Valid Login

---
+++ Condition,DecisonMaking/LoginValidation.py
@@ -0,0 +1,11 @@
+user = "Srivarshini"
+passw = "varsha@123"
+
+username = input("Enter Username: ")
+password = input("Enter password: ")
+
+if(user==username and password==passw):
+    print("Login Successfull")
+else:
+    print("Invalid Credintals")
+
